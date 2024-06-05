a81478eaa1dfb13d571fca5de22e34dde3c02df8
P.Ksonzhik <P.Ksonzhik@cbt.by>
2024-06-05 13:08:23 +0300

    HW-14

--- module_14_mvc/homework/models.py
+++ module_14_mvc/homework/models.py
@@ -1,6 +1,10 @@
 import sqlite3
 from typing import Any, Optional, List
 
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired
+
 DATA: List[dict] = [
     {'id': 0, 'title': 'A Byte of Python', 'author': 'Swaroop C. H.'},
     {'id': 1, 'title': 'Moby-Dick; or, The Whale', 'author': 'Herman Melville'},
@@ -8,12 +12,21 @@ DATA: List[dict] = [
 ]
 
 
-class Book:
+class AddBookForm(FlaskForm):
+    book_title = StringField(validators=[InputRequired()])
+    author_name = StringField(validators=[InputRequired()])
 
-    def __init__(self, id: int, title: str, author: str) -> None:
+
+class ShowAuthorForm(FlaskForm):
+    author = StringField(validators=[InputRequired()])
+
+
+class Book:
+    def __init__(self, id: int, title: str, author: str, count: int = 0) -> None:
         self.id: int = id
         self.title: str = title
         self.author: str = author
+        self.count: int = count
 
     def __getitem__(self, item: str) -> Any:
         return getattr(self, item)
@@ -36,7 +49,8 @@ def init_db(initial_records: List[dict]) -> None:
                 CREATE TABLE `table_books` (
                     id INTEGER PRIMARY KEY AUTOINCREMENT, 
                     title TEXT, 
-                    author TEXT, 
+                    author TEXT,
+                    count INTEGER default 0
                 )
                 """
             )
@@ -61,3 +75,58 @@ def get_all_books() -> List[Book]:
             """
         )
         return [Book(*row) for row in cursor.fetchall()]
+
+
+def add_book(book: Book) -> None:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO table_books (title, author) 
+            VALUES (?, ?)
+            """, (book.title, book.author))
+
+
+def get_books_by_author(author: str) -> List[Book]:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * from `table_books`
+            WHERE author = ?
+            """, (author,))
+        return [Book(*row) for row in cursor.fetchall()]
+
+
+def get_book_by_id(book_id: int) -> Book:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        cursor.execute(
+            """
+            SELECT * from `table_books`
+            WHERE id = ?
+            """, (book_id,))
+        return Book(*cursor.fetchone())
+
+
+def update_count_many_books(books: List[Book]) -> None:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        query = """
+            UPDATE `table_books` 
+            SET count= :count 
+            WHERE id= :id
+        """
+        parameters = [{"id": i_book.id, "count": i_book.count + 1} for i_book in books]
+        cursor.executemany(query, parameters)
+
+
+def update_count_book(book: Book) -> None:
+    with sqlite3.connect('table_books.db') as conn:
+        cursor: sqlite3.Cursor = conn.cursor()
+        query = """
+            UPDATE table_books 
+            SET count= :count 
+            WHERE id= :id
+        """
+        cursor.execute(query, {"id": book.id, "count": book.count + 1})
